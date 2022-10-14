1c7a3fb15afa3c65a2d622f29de75072accd1804
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-14 11:17:09 -0500

    mulitple properties in store

--- js/reactive-set.js
+++ js/reactive-set.js
@@ -1,21 +1,36 @@
-let price = 5;
-let quantity = 2;
+const depsMap = new Map();
+
+function track(key){
+	let dep = depsMap.get(key);
+
+	if(!dep) {
+		depsMap.set(key, (dep = new Set())) // No dep yet, so let's create one
+	}
+
+	dep.add(effect); // Add this effect
+	// Since it's a set, it won't add the effect again if it already exists
+}
+
+function trigger(key){
+	let dep = depsMap.get(key); // Get the dep for this key
+	if(dep){
+		dep.forEach(effect => effect()) // If it exists, run each effect
+	}
+}
+
+let product = {price: 5, quantity: 2};
 let total = 0;
 
-let dep = new Set(); // To store our effects
+let effect = ()=> {
+	total = product.price * product.quantity;
+}
 
-let effect = () => {
-	total = price * quantity;
-	console.log(total);
-};
 
-function track() { dep.add(effect)} // Add the code
-
-function trigger() {dep.forEach(effect=> effect())} // Re-run all the code in storage
-
-track();
+track('quantity');
 effect();
+console.log('total: ', total);
 
-quantity = 3;
-
-trigger()
+product.quantity = 3;
+console.log('product.quantity: ', product.quantity);
+trigger('quantity');
+console.log('total: ', total);
