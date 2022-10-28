84e5ff852844166c21d0d4c5af2650c21f72d373
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-28 16:27:36 -0500

    Turn an array of numbers into a long string of all those numbers

--- js/exercicesReducer.js
+++ js/exercicesReducer.js
@@ -4,6 +4,12 @@ const total = arr.reduce((acc, elem) => acc + elem, 0);
 console.log("total: ", total);
 
 // 2) Turn an array of numbers into a long string of all those numbers.
+const stringConcat = arr.reduce(
+	(acc, elem) => acc.toString() + elem.toString(),
+	""
+);
+console.log("stringConcat: ", stringConcat);
+
 // 3) Turn an array of voter objects into a count of how many people voted
 // 4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
 // 5) Given an array of arrays, flatten them into a single array
