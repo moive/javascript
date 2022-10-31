8a4c3af802c5147cbf5e5e1d52559c698ec98949
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-31 15:43:42 -0500

    memoization first example

--- js/memoization.js
+++ js/memoization.js
@@ -3,8 +3,16 @@ const memoization = (fn) => {
     let results = {};
     return (arg) => {
         if (!results[arg]) {
+            console.log(`results[${arg}] not saved`);
             results[arg] = fn(arg);
+        }
+        else {
+            console.log(`results[${arg}] saved`);
         }
         return results[arg];
     };
 };
+const mult2 = memoization((x) => x * 2);
+console.log(mult2(1));
+console.log(mult2(1));
+console.log(mult2(1));
--- src/memoization.ts
+++ src/memoization.ts
@@ -3,8 +3,18 @@ const memoization = (fn: Function) => {
 
 	return (arg: any) => {
 		if (!results[arg]) {
+			console.log(`results[${arg}] not saved`);
 			results[arg] = fn(arg);
+		} else {
+			console.log(`results[${arg}] saved`);
 		}
+
 		return results[arg];
 	};
 };
+
+const mult2 = memoization((x: number) => x * 2);
+
+console.log(mult2(1));
+console.log(mult2(1));
+console.log(mult2(1));
