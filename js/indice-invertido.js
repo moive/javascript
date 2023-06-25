a4fe7357174e9ad730488425b18e6f2aab1075e1
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-06-24 21:21:35 -0500

    inverted index

---
+++ js/indice-invertido.js
@@ -0,0 +1,14 @@
+"use strict";
+let documents = [
+    "Hola mundo me gusta python",
+    "Con la programación puedes hacerlo todo",
+    "Con python puedo crear servicios web",
+    "pildoras de programación moive!!!",
+];
+const results = [];
+for (const doc in documents) {
+    if (documents[doc].includes("python")) {
+        results.push(documents[doc]);
+    }
+}
+console.log({ results });
