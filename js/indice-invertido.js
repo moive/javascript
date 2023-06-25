ad5c471ebe51bc74808c38f8e838743dd8e38d83
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-06-24 22:41:46 -0500

    example optimize code

--- js/indice-invertido.js
+++ js/indice-invertido.js
@@ -12,3 +12,28 @@ for (const doc in documents) {
     }
 }
 console.log({ results });
+let users = [];
+let products = [];
+const keyBy = (arr, key) => arr.reduce((acc, el) => {
+    acc[el[key]] = el;
+    return acc;
+}, {});
+const elem = 50000;
+for (let i = 0; i < elem; i++) {
+    users.push({ id: i, name: `name-${i}` });
+}
+for (let i = 0; i < elem; i++) {
+    products.push({
+        id: i,
+        name: `product-name-${i}`,
+        user_id: Math.floor(Math.random() * elem),
+    });
+}
+const keyedUsers = keyBy(users, "id");
+console.time("1");
+const mixed = products.map((x) => ({
+    ...x,
+    user: keyedUsers[x.user_id], //users.find((y: any) => y.id === x.user_id),
+}));
+console.timeEnd("1");
+console.log(mixed[0]);
--- src/indice-invertido.ts
+++ src/indice-invertido.ts
@@ -12,3 +12,35 @@ for (const doc in documents) {
 	}
 }
 console.log({ results });
+
+let users: any = [];
+let products: any = [];
+
+const keyBy = (arr: any, key: any) =>
+	arr.reduce((acc: any, el: any) => {
+		acc[el[key]] = el;
+		return acc;
+	}, {});
+
+const elem = 50000;
+for (let i = 0; i < elem; i++) {
+	users.push({ id: i, name: `name-${i}` });
+}
+
+for (let i = 0; i < elem; i++) {
+	products.push({
+		id: i,
+		name: `product-name-${i}`,
+		user_id: Math.floor(Math.random() * elem),
+	});
+}
+
+const keyedUsers = keyBy(users, "id");
+
+console.time("1");
+const mixed = products.map((x: any) => ({
+	...x,
+	user: keyedUsers[x.user_id], //users.find((y: any) => y.id === x.user_id),
+}));
+console.timeEnd("1");
+console.log(mixed[0]);
